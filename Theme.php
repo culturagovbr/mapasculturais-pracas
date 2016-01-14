d889179a98d60c109f1efd6203dbeab07c4bb251
Gabriel Avaloni <avaloni@users.noreply.github.com>
2016-01-14 16:34:50 -0200

    adicionando o hook na consulta para só buscar os espaços do tipo Praça CEU

--- Theme.php
+++ Theme.php
@@ -22,7 +22,7 @@ class Theme extends BaseV1\Theme{
             'site: by the site owner' => 'pelo Ministério da Cultura',
 
             'home: title' => "Bem-vind@!",
-            'home: abbreviation' => "CEUS",
+            'home: abbreviation' => "Praça",
             'home: welcome' => "Página para visualização e cadastro de atividades dos CEUS.",
             'home: events' => "Você pode pesquisar eventos culturais nos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente.",
             'home: spaces' => "Procure por espaços culturais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades artísticas e culturais.",
@@ -31,7 +31,11 @@ class Theme extends BaseV1\Theme{
             'search: verified' => "Verificados",
 
             'entities: Space' => 'Praça',
-            'entities: Spaces' => 'Praças'
+            'entities: Spaces' => 'Praças',
+            'entities: My Spaces'=> 'Minhas Praças',
+            'entities: My spaces'=> 'Minhas praças',
+            'entities: no registered spaces'=> 'nenhuma praça cadastrada',
+            'entities: no spaces'=> 'nenhuma praça'
         ];
     }
 
@@ -42,6 +46,11 @@ class Theme extends BaseV1\Theme{
     function _init() {
         parent::_init();
         $app = App::i();
+
+        $app->hook('API.<<*>>(space).query', function(&$data, &$select_properties, &$dql_joins, &$dql_where) {
+            $dql_where .= ' AND e._type = 132';
+        });
+
         $app->hook('view.render(<<*>>):before', function() use($app) {
             $this->_publishAssets();
         });
